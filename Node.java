f7c9e0f025ddc9d2856aabe9feddbfdbcd3f8628
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-08-11 10:52:05 +0530

    first commit UC1 create a LinkedList insert values

---
+++ Node.java
@@ -0,0 +1,10 @@
+
+public class Node {
+
+	int data;
+	Node next;
+	
+
+	}
+
+
---
+++ Runner.java
@@ -0,0 +1,17 @@
+//import java.util.LinkedList;
+
+public class Runner {
+
+	public static void main(String[] args) {
+	
+		linklist list = new linklist();
+		list.insert(56);
+		list.insert(30);
+		list.insert(70);
+		
+		
+		list.show();
+
+	}
+
+}
